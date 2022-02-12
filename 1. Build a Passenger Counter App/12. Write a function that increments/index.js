0062362312e65fa54460e6f20dc6816af13d4372
Joseph <softjoe40@gmail.com>
2022-02-12 10:02:14 +0100

    now done with passanger counter app

---
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -0,0 +1,18 @@
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function lapsfun() {
+    lapsCompleted = lapsCompleted + 1;
+    // console.log(lapsCompleted)
+}
+
+
+
+lapsfun()
+lapsfun()
+lapsfun()
+lapsfun()
+
+console.log(lapsCompleted)
